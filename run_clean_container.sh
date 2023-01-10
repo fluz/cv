cb17ea1783f4cf6339757c5c18bb2a3712529d81
Fernando Luz <fernando.luz@talkdesk.com>
2023-01-10 23:56:21 +0000

    add correct label for docker compose service

---
+++ run_clean_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Removing previous execution"
+rm -rf build/*
+
+echo "Removing all containers"
+docker ps -a -q | xargs docker rm
+echo "Removing all images"
+docker images -a -q | xargs docker rmi -f
+echo "Execute last version"
+docker-compose run cv
